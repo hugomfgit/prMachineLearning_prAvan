6b1154f7b915690901d92e6e96c75ac6bc431e41
Hugo Martí Fernández <hugomfgit@gmail.com>
2023-03-07 11:38:53 +0100

    Constructor de Kmeans Hecho

---
+++ src/main/java/es/uji/al415617/Kmeans.java
@@ -0,0 +1,27 @@
+package es.uji.al415617;
+
+import java.util.Random;
+
+public class Kmeans {
+
+    int numClusters;
+    int numIterations;
+    long seed;
+
+    public Kmeans(int numClusters, int numIterations, long seed){
+        this.numClusters=numClusters;
+        this.numIterations=numIterations;
+        this.seed=seed;
+    }
+
+
+    public void train(Table datos){
+
+       
+
+
+    }
+
+
+
+}
