6d638417d19eac71d83840fed1e604a487e6c33e
Hugo415617 <hugomfgit@gmail.com>
2023-04-01 15:19:23 +0200

    todos los métodos hechos, queda pruebas y canciones

--- src/main/java/es/uji/al415617/RecSys.java
+++ src/main/java/es/uji/al415617/RecSys.java
@@ -1,5 +1,6 @@
 package es.uji.al415617;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -8,8 +9,9 @@ public class RecSys {
     Algorithm algoritmo;
     Table tablaDeDatos;
     List<String> nombresDeItems;
-
     Map<Integer, Integer> mapaGrupos;
+
+    List<Integer> indicesSimilares;
 
     public RecSys(Algorithm algorithm){
         this.algoritmo=algorithm;
@@ -26,7 +28,11 @@ public class RecSys {
     }
 
     public List<String> recommend(String nameLikedItem, int numRecommendations){
-
+        int indiceNombreDado = findName(nameLikedItem);
+        Row filaDelNombre = tablaDeDatos.filas.get(indiceNombreDado);
+        int indiceCentroide = (int) algoritmo.estimate(filaDelNombre.data);
+        selectItems(indiceNombreDado, indiceCentroide, numRecommendations);
+        return getNamesSelectedItems();
     }
 
     private void estimate(){
@@ -35,10 +41,27 @@ public class RecSys {
         }
     }
 
-    private int findName(String nameItem);
+    private int findName(String nameItem){
+        int indice=-1;
+        for(int i=0; indice<nombresDeItems.size(); indice++)
+            if(nombresDeItems.get(indice).equals(nameItem))
+                indice=i;
+        return indice;
+    }
 
-    private void selectItems(int idxLikedItem, int labelLikedItem, int numRec);
-
-    private List<String> getNamesSelectedItems();
+    private void selectItems(int idxLikedItem, int labelLikedItem, int numRec){
+        for(int indice=0; indice<tablaDeDatos.filas.size(); indice++){
+            if(labelLikedItem==((int) algoritmo.estimate(tablaDeDatos.filas.get(indice).data)) & numRec!=0 & idxLikedItem!=indice){
+                indicesSimilares.add(indice);
+                numRec--;
+            }
+        }
+    }
+    private List<String> getNamesSelectedItems(){
+        List<String> nombresSeleccionados = new ArrayList<>();
+        for(int indice:indicesSimilares)
+            nombresSeleccionados.add(nombresDeItems.get(indice));
+        return nombresSeleccionados;
+    }
 
 }
